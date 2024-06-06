cec1833054b184dd6a48e2511f189b31d76a7d6a
aaron <redskin.rox@gmail.com>
2024-06-06 12:10:55 -0400

    created the menu

--- librarymanagement/src/main/java/library/Main.java
+++ librarymanagement/src/main/java/library/Main.java
@@ -3,10 +3,11 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    private static final String url = "jdbc:mysql://localhost:3306/inventory_management";
+    private static final String url = "jdbc:mysql://localhost:3306/library";
     private static final String username = "root";
-    private static final String password = "rootroot";
+    private static final String password = "maplestory";
     public static Scanner scanner = new Scanner(System.in);
+    public static User user;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
@@ -17,6 +18,7 @@ public class Main {
         try(Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("1. Sign up 2. Sign in ");
             int choice = scanner.nextInt();
+            scanner.nextLine();
             if (choice == 1) {
                 System.out.println("Enter username: ");
                 String username1 = scanner.nextLine();
@@ -31,7 +33,7 @@ public class Main {
             String usern = scanner.nextLine();
             System.out.println("Enter password: ");
             String passw = scanner.nextLine();
-            User user = authenticateUser(connection, usern, passw);
+            user = authenticateUser(connection, usern, passw);
 
             if (user != null) {
                 if ("librarian".equalsIgnoreCase(user.getRole())) {
@@ -61,14 +63,14 @@ public class Main {
 
     public static User authenticateUser(Connection connection, String usern, String passw) {
         // Verify if user exists with correct username and password
-        String readQuery = "Select * FROM users WHERE username = (?) AND password = (?)";
+        String readQuery = "Select * FROM user WHERE username = (?) AND password = (?)";
         try(PreparedStatement preparedStatement = connection.prepareStatement(readQuery)) {
             preparedStatement.setString(1, usern);
             preparedStatement.setString(2, passw);
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
                 User user = new User();
-                user.setId(resultSet.getInt("id"));
+                user.setId(resultSet.getInt("userId"));
                 user.setUsername(resultSet.getString("username"));
                 user.setPassword(resultSet.getString("password"));
                 user.setRole(resultSet.getString("role"));
@@ -78,6 +80,45 @@ public class Main {
             e.printStackTrace();
         }
         return null;
+    }
+    public void libraryMenu(){
+        int choice = 0;
+        //the choices if the user is an admin
+        if(user.getRole().equals("librarian")){
+            do{
+                System.out.println("librarian Menu:");
+                System.out.println("1) View checked out books");
+                System.out.println("2) View books");
+                System.out.println("3) View overdue Books");
+                System.out.println("4) Total fees");
+                System.out.println("5) exit");
+//                choice = scanner.nextInt();
+//                scanner.nextLine();
+//                switch(choice){
+//                    case 1 ->
+//                    case 2 ->
+//                    case 3 ->
+//                    case 4 ->
+//                    case 5 ->
+//
+//                }
+            }while(choice!= 5);
+
+        }else{
+            //choices if the user isnt an librarian
+            do{
+                System.out.println("User Menu:");
+                System.out.println("1) View checked out books");
+                System.out.println("2) View fees");
+                System.out.println("3) Check out book");
+
+//                choice = myObj.nextInt();
+//                myObj.nextLine();
+//                switch(choice){
+//                    case 1 ->
+//                    case 2 ->
+//                }
+            }while(choice!= 3);
     }
 
 }
